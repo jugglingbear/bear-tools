f3cef46b7edc4a0be2ef75f27395c43856c3dd90
Sean Foley <sean.foley.engr@gmail.com>
2026-03-11 11:53:26 -0700

    Updated get_current_ssid_macos15 with prerequisite instructions

--- src/bear_tools/macos_utils.py
+++ src/bear_tools/macos_utils.py
@@ -2,6 +2,8 @@
 macOS-specific utilities
 """
 
+# TODO: Update to use platform.mac_ver() and have one unified get-ssid API
+
 import platform
 import re
 import subprocess
@@ -30,13 +32,24 @@ def get_current_ssid_macos15(interface: str = 'en0') -> str | None:
     """
     Get the currently-connected SSID (macOS 15 and newer)
 
+    Note:
+        On newer versions of macOS 15.x, this command requires running the following command once to get set up:
+        sudo ipconfig setverbose 1
+
+    Example without setverbose:
+        ipconfig getsummary en0 | grep SSID
+            BSSID : <redacted>
+            SSID : <redacted>
+
     :param interface: The wireless interface to use
     :return: The currently-connected SSID if found; None otherwise
     """
 
     try:
         output: str = subprocess.check_output(['ipconfig', 'getsummary', f'{interface}']).decode()
-        regex: str = r'\n\s+SSID : ([\x20-\x7E]{1,32})'
+        # Require the captured ASCII token to run right up to the newline.
+        # This prevents partial matches like "Caf" from "CaféNet".
+        regex: str = r'\n\s+SSID : ([\x20-\x7E]{1,32})(?=\n)'
         match = re.search(regex, output)
         return match.group(1) if match else None
     except subprocess.CalledProcessError:
